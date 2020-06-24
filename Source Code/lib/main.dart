aeb223bda47e08ad66f503a3dfd080ced0a43bef
alekya pochampally <S536776@nwmissouri.edu>
2020-06-23 19:52:21 -0500

    changes in main.dart

--- Source Code/lib/main.dart
+++ Source Code/lib/main.dart
@@ -1,23 +1,24 @@
-import 'package:carparking/ui/pages/login/login.dart';
-import 'package:carparking/ui/pages/reserve/reserve.dart';
+import 'package:carparking/authentication/base_authentication_impl.dart';
+import 'package:carparking/ui/pages/change_password.dart';
+import 'package:carparking/ui/pages/splash_screen_page.dart';
 import 'package:flutter/material.dart';
 
-import 'ui/pages/login/login.dart';
+import 'ui/pages/forgot_password_page.dart';
+import 'ui/pages/login_page.dart';
+import 'ui/pages/signup_page.dart';
 
 void main() {
-  runApp(StartApplication());
+  runApp(MaterialApp(
+    title: 'Campus Parking',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+    home: SplashScreenPage(authentication: BaseAuthenticationImpl()),
+    routes: {
+      // Login and signup related
+      LoginPage.route: (context) => LoginPage(),
+      ForgotPasswordPage.route: (context) => ForgotPasswordPage(),
+      SignUpPage.route: (context) => SignUpPage(),
+      ChangePasswordPage.route: (context) => ChangePasswordPage(),
+    },
+  ));
 }
-
-class StartApplication extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Car Parking")),
-        body: LoginPage()        //Reserve()
-      ),
-    );
-  }
-}
-
-
