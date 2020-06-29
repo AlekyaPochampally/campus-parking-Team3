e35cd3aa49065c11dac9e066bf507424793bd245
manideepchamala <54381062+manideepchamala@users.noreply.github.com>
2020-06-28 22:15:38 -0500

    added annotations/VerifyRequestBody.java and aspects/AspectValidateRequestBody.java

---
+++ Source Code/main/java/edu/nwmissouri/mobile/carparking/annotations/VerifyRequestBody.java
@@ -0,0 +1,14 @@
+package edu.nwmissouri.mobile.carparking.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface VerifyRequestBody {
+
+    boolean enabled() default true;
+
+}
