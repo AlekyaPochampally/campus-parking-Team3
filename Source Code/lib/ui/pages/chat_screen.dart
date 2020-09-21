5642c4f05f59c4459c8c9b5bbf7dd71f7172091e
alekya pochampally <S536776@nwmissouri.edu>
2020-09-20 21:18:17 -0500

    Added chat screen and other existing screens

---
+++ Source Code/lib/ui/pages/chat_screen.dart
@@ -0,0 +1,204 @@
+import 'package:campusparking/ui/widgets/app_bar.dart';
+import 'package:campusparking/ui/widgets/constants.dart';
+import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = Firestore.instance;
+FirebaseUser loggedInUser;
+
+class ChatScreen extends StatefulWidget {
+  static const String route = 'chat_screen';
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+
+  String messageText;
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+//  void getMessages()async {
+//   final messages = await _firestore.collection('messages').getDocuments();
+//   for(var message in messages.documents)
+//       {
+//         print(message.data);
+//       }
+//  }
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.documents) {
+        print(message.data);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar:ApplicationBar(),
+      body: SafeArea(
+        child: Column(
+
+//          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Live Chat',
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Get immidiate assistance by aksing your query in below chat window',
+           //     style: TextStyle(fontSize: 20),
+              ),
+            ),
+            MessageStream(),
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: messageTextController,
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      messageTextController.clear();
+                      _firestore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedInUser.email,
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _firestore.collection('messages').snapshots(),
+        // ignore: missing_return
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.lightBlueAccent,
+              ),
+            );
+          }
+          final messages = snapshot.data.documents.reversed;
+          List<MessageBubble> messageBubbles = [];
+          for (var message in messages) {
+            final messageText = message.data['text'];
+            final messageSender = message.data['sender'];
+            final currentUser = loggedInUser.email;
+            if (currentUser == messageSender) {
+              //msg from logged in user
+
+            }
+            final messageWidget = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+              isMe: currentUser == messageSender,
+            );
+            messageBubbles.add(messageWidget);
+          }
+          return Expanded(
+            child: ListView(
+              reverse: true,
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              children: messageBubbles,
+            ),
+          );
+        });
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text, this.isMe});
+
+  final String sender, text;
+  final bool isMe;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment:
+          isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(sender,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.grey[100],
+                )),
+            Material(
+              borderRadius: isMe
+                  ? BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0))
+                  : BorderRadius.only(
+                  topRight: Radius.circular(30.0),
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0)),
+              elevation: 5.0,
+              color: isMe ? Colors.grey[500] : Colors.grey[300],
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Text(text,
+                    style: TextStyle(
+                      color: isMe ? Colors.white : Colors.black,
+                      fontSize: 20.0,
+                    )),
+              ),
+            ),
+          ],
+        ));
+  }
+}
---
+++ Source Code/lib/ui/pages/login_page.dart
@@ -0,0 +1,162 @@
+
+import 'package:flutter/material.dart';
+import 'package:campusparking/ui/pages/signup_page.dart';
+import 'package:campusparking/ui/pages/user_home_page.dart';
+import 'package:campusparking/ui/widgets/app_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:campusparking/ui/pages/Faq_page.dart';
+
+import 'forgot_password_page.dart';
+
+class LoginPage extends StatefulWidget {
+  static const String route = '/login';
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final _auth = FirebaseAuth.instance;
+
+  String _email;
+  String _password;
+
+//  @override
+//  void initState() {
+//    super.initState();
+//  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: ApplicationBar(),
+        body: SafeArea(
+            child: Form(
+
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Campus Parking',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+
+                // Username
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email Id',
+                    ),
+                    onChanged: (value) {
+                      _email = value;
+                    },
+//                    validator: (value) {
+//                      if (value.isEmpty) {
+//                        return 'Provide an email id';
+//                      }
+//                      _formKey.currentState.save();
+//                      return null;
+//                    },
+//                    onSaved: (value) {
+//                      _email = value;
+//                    },
+                  ),
+                ),
+
+                // Password
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                    onChanged: (value) {
+                      _password = value;
+                    },
+//                    validator: (value) {
+//                      if (value.isEmpty) {
+//                        return 'Provide your password';
+//                      }
+//                      _formKey.currentState.save();
+//                      return null;
+//                    },
+//                    onSaved: (value) {
+//                      _password = value;
+//                    },
+                  ),
+                ),
+
+                // Login button
+                Container(
+                  height: 55,
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Column(
+                    children: [
+                      RaisedButton(
+                        onPressed: () async {
+                          try {
+                            print(_email);
+                            print(_password);
+
+                            final user = await _auth.signInWithEmailAndPassword(
+                                email: _email, password: _password);
+                            if (user != null)
+                              Navigator.pushNamed(context,UserHomePage.route);
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                        child: Text(
+                          'Login',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Signup information
+                Container(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FlatButton(
+                        onPressed: () => Navigator.of(context)
+                            .pushNamed(ForgotPasswordPage.route),
+                        child: Text('Forgot Password')),
+                    FlatButton(
+                        onPressed: () =>
+                            Navigator.of(context).pushNamed(SignUpPage.route),
+                        child: Text('Sign Up')),
+                    FlatButton(
+                        onPressed: () =>
+                            Navigator.of(context).pushNamed(FaQPage.route),
+                        child: Text('FAQ'))
+                  ],
+                ))
+
+              ],
+            ),
+          ),
+        )));
+  }
+}
