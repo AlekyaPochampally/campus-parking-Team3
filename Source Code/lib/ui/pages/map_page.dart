85523b6d0883683aada65fbf261210dd833e67ac
Manisha <manisha.mengani468@gmail.com>
2020-10-25 21:49:14 -0500

    updated map_page.dart

--- Source Code/lib/ui/pages/map_page.dart
+++ Source Code/lib/ui/pages/map_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'ParkingLotList.dart';
-import 'package:campusparking/ui/widgets/app_bar.dart';
 
+// ignore: camel_case_types
 class mapPage extends StatefulWidget {
   static const String route = '/map';
 
@@ -30,7 +30,7 @@ class _mapPageState extends State<mapPage> {
           draggable: false,
           infoWindow: InfoWindow(
             title: element.lotNumber,
-            snippet: element.lotaddress,
+            snippet: element.availableParkingLots.toString(),
           ),
           position: element.locationCoords));
     });
@@ -45,92 +45,93 @@ class _mapPageState extends State<mapPage> {
     }
   }
 
-  // Widget _LotsList(int index) {
-  //   return AnimatedBuilder(
-  //     animation: _pageController,
-  //     builder: (BuildContext context, Widget widget) {
-  //       double value = 1;
-  //       if (_pageController.position.haveDimensions) {
-  //         value = _pageController.page - index;
-  //         value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
-  //       }
-  //       return Center(
-  //         child: SizedBox(
-  //           height: Curves.easeInOut.transform(value) * 125.0,
-  //           width: Curves.easeInOut.transform(value) * 350.0,
-  //           child: widget,
-  //         ),
-  //       );
-  //     },
-  //     child: InkWell(
-  //         onTap: () {
-  //           // moveCamera();
-  //         },
-  //         child: Stack(children: [
-  //           Center(
-  //               child: Container(
-  //                   margin: EdgeInsets.symmetric(
-  //                     horizontal: 10.0,
-  //                     vertical: 20.0,
-  //                   ),
-  //                   height: 125.0,
-  //                   width: 275.0,
-  //                   decoration: BoxDecoration(
-  //                       borderRadius: BorderRadius.circular(10.0),
-  //                       boxShadow: [
-  //                         BoxShadow(
-  //                           color: Colors.black54,
-  //                           offset: Offset(0.0, 4.0),
-  //                           blurRadius: 10.0,
-  //                         ),
-  //                       ]),
-  //                   child: Container(
-  //                       decoration: BoxDecoration(
-  //                           borderRadius: BorderRadius.circular(10.0),
-  //                           color: Colors.white),
-  //                       child: Row(children: [
-  //                         Container(
-  //                             height: 90.0,
-  //                             width: 90.0,
-  //                             decoration: BoxDecoration(
-  //                                 borderRadius: BorderRadius.only(
-  //                                     bottomLeft: Radius.circular(10.0),
-  //                                     topLeft: Radius.circular(10.0)),
-  //                                 image: DecorationImage(
-  //                                     image:
-  //                                         NetworkImage(lots[index].thumbNail),
-  //                                     fit: BoxFit.cover))),
-  //                         SizedBox(width: 5.0),
-  //                         Column(
-  //                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //                             crossAxisAlignment: CrossAxisAlignment.start,
-  //                             children: [
-  //                               Text(
-  //                                 lots[index].lotNumber,
-  //                                 style: TextStyle(
-  //                                     fontSize: 12.5,
-  //                                     fontWeight: FontWeight.bold),
-  //                               ),
-  //                               Text(
-  //                                 lots[index].lotaddress,
-  //                                 style: TextStyle(
-  //                                     fontSize: 12.0,
-  //                                     fontWeight: FontWeight.w600),
-  //                               ),
-  //                               Container(
-  //                                 width: 170.0,
-  //                                 child: Text(
-  //                                   lots[index].totalParkingLots,
-  //                                   style: TextStyle(
-  //                                       fontSize: 11.0,
-  //                                       fontWeight: FontWeight.w300),
-  //                                 ),
-  //                               )
-  //                             ])
-  //                       ]))))
-  //         ])),
-  //   );
-  // }
+  // ignore: non_constant_identifier_names
+  Widget _LotsList(int index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget widget) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = _pageController.page - index;
+          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 125.0,
+            width: Curves.easeInOut.transform(value) * 350.0,
+            child: widget,
+          ),
+        );
+      },
+      child: InkWell(
+          onTap: () {
+            // moveCamera();
+          },
+          child: Stack(children: [
+            Center(
+                child: Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 20.0,
+                    ),
+                    height: 125.0,
+                    width: 275.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            offset: Offset(0.0, 4.0),
+                            blurRadius: 10.0,
+                          ),
+                        ]),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white),
+                        child: Row(children: [
+                          Container(
+                              height: 90.0,
+                              width: 90.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(10.0),
+                                      topLeft: Radius.circular(10.0)),
+                                  image: DecorationImage(
+                                      image:
+                                          NetworkImage(lots[index].thumbNail),
+                                      fit: BoxFit.cover))),
+                          SizedBox(width: 5.0),
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  lots[index].lotNumber,
+                                  style: TextStyle(
+                                      fontSize: 12.5,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  lots[index].lotaddress,
+                                  style: TextStyle(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Container(
+                                  width: 170.0,
+                                  child: Text(
+                                    lots[index].totalParkingLots,
+                                    style: TextStyle(
+                                        fontSize: 11.0,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                )
+                              ])
+                        ]))))
+          ])),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -160,9 +161,9 @@ class _mapPageState extends State<mapPage> {
                 //   itemCount: lots.length,
                 //   itemBuilder: (BuildContext context, int index) {
                 //     return _LotsList(index);
-                //   },
+                //     },
               )),
-          //),
+          //  ),
         ]));
   }
 
